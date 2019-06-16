7ce60c60b1d0f1331d2411fb8247eac39848a298
vikno3 <39916326+vikno3@users.noreply.github.com>
2019-06-16 11:48:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.MzM1ODEyMzIwODM4MDI1MjE2.XQYCLA.NmeOKb25JXefHia96NlTbnJOQfA);
